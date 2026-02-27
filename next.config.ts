2e277faf1953f687576497718f6f27243f7e018b
Darío Martínez Kostyuk <dariomartk@gmail.com>
2026-02-27 16:29:26 +0100

    feat: changed image to 3 year

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    formats: ['image/webp', 'image/avif'],
+    formats: ["image/webp", "image/avif"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    qualities: [75, 85, 90],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 60,
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
   experimental: {
-    optimizePackageImports: ['next/image'],
+    optimizePackageImports: ["next/image"],
   },
 };
 
